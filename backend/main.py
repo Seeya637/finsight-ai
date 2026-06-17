246563aa6fe78340ec1bc8b743672c72508ccc2a
Seeya637 <bagrawal72077@email.com>
2026-06-17 15:21:59 +0530

    feat: production URLs configured

--- backend/main.py
+++ backend/main.py
@@ -12,11 +12,13 @@ app=FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://*.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-
 )
 
 app.include_router(auth_router,prefix="/auth", tags=["Auth"])
